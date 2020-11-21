8615d52629fccf64ae24ed988e57210770a06da1
ahermann15 <58404037+ahermann15@users.noreply.github.com>
2020-11-21 20:50:00 +0100

    ...

--- ConsoleCommands.cs
+++ ConsoleCommands.cs
@@ -8,6 +8,22 @@ namespace ResetHeroes
 {
     public static class ConsoleCommands
     {
+		[CommandLineFunctionality.CommandLineArgumentFunction("resetMainHero", "campaign")]
+		public static string ResetMainHero(List<string> strings)
+		{
+			bool flag = Campaign.Current == null;
+			string result;
+			if (flag)
+			{
+				result = "Campaign is null";
+			}
+			else
+			{
+				ResetHeroesFunctions.ResetMainHero();
 
-    }
+				result = "done" + strings;
+			}
+			return result;
+		}
+	}
 }
--- ResetHeroesFunctions.cs
+++ ResetHeroesFunctions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Helpers;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
@@ -6,7 +7,27 @@ using TaleWorlds.Library;
 
 namespace ResetHeroes
 {
-    internal class ResetHeroesFunctions
+    public static class ResetHeroesFunctions
     {
-    }
+		public static void ResetMainHero()
+		{
+			Hero hero = Hero.MainHero;
+			hero.HeroDeveloper.ClearHero();
+
+			PropertyInfo propertyInfo = typeof(HeroDeveloper).GetProperty("TotalXp");
+			propertyInfo.SetValue(hero.HeroDeveloper, 1, BindingFlags.NonPublic | BindingFlags.Instance, null, null, null);
+
+			HeroHelper.DetermineInitialLevel(hero);
+
+			hero.HeroDeveloper.UnspentAttributePoints = 18;
+			hero.HeroDeveloper.UnspentFocusPoints = 12;
+
+			for (CharacterAttributesEnum characterAttributesEnum = CharacterAttributesEnum.Vigor; characterAttributesEnum < CharacterAttributesEnum.NumCharacterAttributes; characterAttributesEnum++)
+			{
+				hero.SetAttributeValue(characterAttributesEnum, 0);
+			}
+
+			InformationManager.DisplayMessage(new InformationMessage(string.Format("Reset {0}", hero)));
+		}
+	}
 }
